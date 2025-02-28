07fa2c5512d7a7584f6a29366adcb4153b2bdb7f
Achilng <lc13714859479@outlook.com>
2025-02-28 23:12:32 +0800

    交作业了喵

---
+++ 计科1241 黎畅/detect.py
@@ -0,0 +1,2 @@
+# 习惯用命令行，这里是用在命令行的代码
+# python detect.py --weights runs/train/exp13/weights/best.pt --source D:\Dev\CV3-master\test\images --img-size 768 --conf-thres 0.4 --iou-thres 0.5 --hide-labels
